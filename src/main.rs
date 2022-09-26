5b0310c4c7bbe881d98deaed288e277792d52be7
Spencer Warneke <warnespe001@gmail.com>
2022-09-25 23:39:53 -0700

    Some very basic content.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use std::{fmt, io::Write, str::{SplitWhitespace, ParseBoolError}, process::exit, collections::HashMap};
+use std::{fmt, io::Write, process::exit, collections::HashMap};
 use regex::Regex;
 
+// Utility types.
+// These help organize data into something a little easier to grok.
 enum ParsedInput {
     // Meta-commands
     Quit,
@@ -22,6 +24,7 @@ enum ParsedInput {
     Other(String)
 }
 
+// Debug formatter for ParsedInput
 impl fmt::Display for ParsedInput {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
@@ -47,6 +50,7 @@ impl fmt::Display for ParsedInput {
     }
 }
 
+// Wrapper-classes for Vec/HashMap
 struct Inventory {
     items: Vec<(String, String)>
 }
@@ -55,38 +59,54 @@ struct Flags {
     flags: HashMap<String, bool>
 }
 
+// Flags implementation
 impl Flags {
     fn new() -> Flags
     {
         Flags { flags: HashMap::new() }
     }
     
+    // Check to see if a certain flag is both defined and set to true.
     fn is_set(&self, flag: &str) -> bool
     {
         *self.flags.get(flag).unwrap_or(&false)
     }
 
+    // Set the given flag to true in the struct.
     fn set(&mut self, flag: &str) {
         self.set_as(flag, true);
     }
 
+    // Set the given flag to `val` in the struct.
     fn set_as(&mut self, flag: &str, val: bool)
     {
         self.flags.insert(String::from(flag), val);
     }
+
+    fn print_if(&self, flag: &str, val: bool, msg: &str)
+    {
+        if self.is_set(flag) == val
+        {
+            println!("{}", msg);
+        }
+    }
 }
 
+// function pointer typedef so that we can map strings to room functions
 type Room = fn(&mut Inventory, &mut Flags) -> String;
 
+// Inventory implementation
 impl Inventory {
     fn new() -> Inventory {
         Inventory { items: vec![] }
     }
 
+    // Adds the item to the vec
     fn add(&mut self, item: &str, desc: &str) {
         self.items.push((String::from(item), String::from(desc)));
     }
 
+    // removes the item from the vec, if it exists
     fn remove(&mut self, item: &str) {
         match self.find(item) {
             Some(i) => self.items.retain(|(itm, _)| itm != item),
@@ -94,6 +114,7 @@ impl Inventory {
         };
     }
 
+    // Returns an Option containing the index of the item if it was found
     fn find(&self, target_item: &str) -> Option<usize> {
         for (i, tupl) in self.items.iter().enumerate() {
             if target_item == tupl.0 {
@@ -103,6 +124,7 @@ impl Inventory {
         None
     }
 
+    // Returns a bool indicating if the vec contains an item with the given name
     fn has(&self, target_item: &str) -> bool {
         self.find(target_item).is_some()
     }
@@ -118,6 +140,8 @@ impl fmt::Display for Inventory {
     }
 }
 
+// Essentially a wrapper for stdin().read_line().
+// Panics if stdout().flush() fails, for some weird reason.
 fn get_user_input() -> Result<ParsedInput, ()> {
     let mut line = String::new();
     print!("> ");
@@ -127,6 +151,7 @@ fn get_user_input() -> Result<ParsedInput, ()> {
     Ok::<ParsedInput, ()>(parse_input(line))
 }
 
+// Parse messy, vague human language into easy-to-deal-with data.
 fn parse_input(s: String) -> ParsedInput {
     // ...this sucks to have to do.
     // This would probably just be easier with regex, but this is what I've resigned myself to, I guess.
@@ -168,33 +193,84 @@ fn parse_input(s: String) -> ParsedInput {
 }
 
 fn test_room(inv: &mut Inventory, flags: &mut Flags) -> String {
+    // Exposition
+    println!("You find yourself standing inside of a developer's test room.");
+    flags.print_if("test_room_got_golden_key", false, "The room is bare, except for a small golden key gleaming gently in the middle of the room.");
     println!("To the north is Room A.");
 
-    if !flags.is_set("test_room_intro")
-    {
-        println!("This should only show the first time you enter test_room.");
-        flags.set("test_room_intro");
-    }
-
+    // Process user input
     String::from(match get_user_input() {
         Ok(ParsedInput::North) => "room_a",
         Ok(ParsedInput::Look(_)) => "test_room",
         Ok(ParsedInput::Inv) => { println!("{}", inv); "test_room" },
         Ok(ParsedInput::Quit) => { exit(0) },
+        Ok(ParsedInput::Get(item)) => {
+            if item.contains("key") {
+                flags.set("test_room_got_golden_key");
+                inv.add("Golden Key", "A quaint key with an irresistable luster.");
+                println!("You pick up the gold key.");
+            }
+            "test_room"
+        }
         _ => "test_room"
     })
 }
 
 fn room_a(inv: &mut Inventory, flags: &mut Flags) -> String {
-    println!("You're in Room A! That's pretty neat!");
-    if inv.has("Golden Key")
-    {
-        inv.remove("Golden Key");
-        inv.add("Sword", "You could do some real damage with this.");
-        println!("You use the Golden Key from Room B to open a chest. You found a sword inside!");
+    // Exposition
+    println!("You find yourself standing inside of Room A. Very clearly distinct from the last room. This one has a name!");
+    flags.print_if("room_a_opened_chest", false, "A chest sits alone in a dark corner of the room.");
+    println!("To the south is the test room.");
+
+    // Helper functions
+    fn open_chest(inv: &mut Inventory, flags: &mut Flags) {
+        if !flags.is_set("room_a_opened_chest") 
+        {
+            if inv.has("Golden Key") 
+            {
+                inv.remove("Golden Key");
+                flags.set("room_a_opened_chest");
+                println!("You opened the chest, and found a sword inside!");
+                inv.add("Sword", "You could do some damage with this.");
+            } 
+            else 
+            {
+                println!("The chest is locked. Maybe there's a key somewhere?");
+            }
+        } 
+        else 
+        {
+            println!("The chest is already open. Don't you remember the cool sword you got?");
+        }
     }
 
-    String::from("room_a")
+    // Process user input
+    String::from(match get_user_input() {
+        Ok(ParsedInput::South) => "test_room",
+        Ok(ParsedInput::Look(_)) => "room_a",
+        Ok(ParsedInput::Inv) => { println!("{}", inv); "room_a" },
+        Ok(ParsedInput::Quit) => { exit(0) },
+        // All of the ways to open the chest
+        Ok(ParsedInput::Use(item)) => {
+            if item.contains("key") {
+                open_chest(inv, flags); 
+            }
+            "room_a"
+        }
+        Ok(ParsedInput::UseOn(item, object)) => {
+            if item.contains("key") && object.contains("chest") {
+                open_chest(inv, flags);
+            }
+            "room_a"
+        }
+        Ok(ParsedInput::Other(text)) => {
+            if text.contains("open") && text.contains("chest") {
+                open_chest(inv, flags);
+            }
+            "room_a"
+        }
+        _ => "room_a"
+    })
 }
 
 fn dead_room(_inv: &mut Inventory, _flags: &mut Flags) -> String
